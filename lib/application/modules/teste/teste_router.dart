52aabc43fd0f503bf1a4d63bdb1304617c14ef8c
dariopmaciel <dariodepaulamaciel@hotmail.com>
2024-08-22 14:13:32 -0300

    montagem das rodas PARA o shelf

--- lib/application/modules/teste/teste_router.dart
+++ lib/application/modules/teste/teste_router.dart
@@ -1,11 +1,12 @@
 
+import 'package:cuidapet_shelf/application/modules/teste/teste_controller.dart';
 import 'package:cuidapet_shelf/application/routes/i_router.dart';
 import 'package:shelf_router/src/router.dart';
 
 class TesteRouter implements IRouter{
   @override
   void configure(Router router) {
-    
+    router.mount("/hello/", TesteController().router.call);
   }
   
 }
