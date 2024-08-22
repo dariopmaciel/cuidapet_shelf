5007f029a16ad8f050eaa81e48714ae7d388fb01
dariopmaciel <dariodepaulamaciel@hotmail.com>
2024-08-22 11:10:56 -0300

    gerenciador de dependencias pronto

--- lib/application/config/application_config.dart
+++ lib/application/config/application_config.dart
@@ -1,26 +1,32 @@
 import 'package:cuidapet_shelf/application/config/database_connection_configuration.dart';
+import 'package:cuidapet_shelf/application/config/service_locator_config.dart';
+
 import 'package:dotenv/dotenv.dart' show load, env;
 import 'package:get_it/get_it.dart';
+import 'package:logger/logger.dart';
 
 class ApplicationConfig {
-  Future<void> _loadEnv() async => load();
-
   Future<void> loadConfigApplication() async {
     await _loadEnv();
-    // final variavel = env['url_banco_de_dados'];
-    // final flutterHome = env['FLUTTER_HOME'];
-
-    // print(variavel);
-    // print(flutterHome);
-    _loadDataBaseConfig() {
-      final databaseConfig = DatabaseConnectionConfiguration(
-        host: env['DATABASE_HOST'] ?? env['databeseHost']!,
-        user: env['DATABASE_USER'] ?? env['databaseUser']!,
-        port: int.tryParse(env['DATABASE_PORT'] ?? env['databasePort']!) ?? 0,
-        password: env['DATABASE_PASSWORD'] ?? env['databasePassword']!,
-        databaseName: env['DATABASE_NAME'] ?? env['databaseName']!,
-      );
-      GetIt.I.registerSingleton(databaseConfig);
-    }
+    _loadDataBaseConfig();
+    _configLogger();
+    _loadDependencies();
   }
+
+  Future<void> _loadEnv() async => load();
+
+  void _loadDataBaseConfig() {
+    final databaseConfig = DatabaseConnectionConfiguration(
+      host: env['DATABASE_HOST'] ?? env['databeseHost']!,
+      user: env['DATABASE_USER'] ?? env['databaseUser']!,
+      port: int.tryParse(env['DATABASE_PORT'] ?? env['databasePort']!) ?? 0,
+      password: env['DATABASE_PASSWORD'] ?? env['databasePassword']!,
+      databaseName: env['DATABASE_NAME'] ?? env['databaseName']!,
+    );
+    GetIt.I.registerSingleton(databaseConfig);
+  }
+
+  void _configLogger() => GetIt.I.registerLazySingleton(() => Logger());
+
+  void _loadDependencies() => configureDependencies();
 }
--- lib/application/database/i_database_connection_impl.dart
+++ lib/application/database/i_database_connection_impl.dart
@@ -1,9 +1,10 @@
 import 'package:cuidapet_shelf/application/config/database_connection_configuration.dart';
+import 'package:injectable/injectable.dart';
 import 'package:mysql1/mysql1.dart';
-
 
 import './i_database_connection.dart';
 
+@LazySingleton(as: IDatabaseConnectionImpl)
 class IDatabaseConnectionImpl extends IDatabaseConnection {
   final DatabaseConnectionConfiguration _configuration;
 
