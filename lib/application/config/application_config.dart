b8f640c37ade048f939e30c12607ca70fbec1295
dariopmaciel <dariodepaulamaciel@hotmail.com>
2024-08-22 10:21:27 -0300

    conf de BD disponiveis para o Get_It-(injectable)

--- lib/application/config/application_config.dart
+++ lib/application/config/application_config.dart
@@ -1,4 +1,6 @@
+import 'package:cuidapet_shelf/application/config/database_connection_configuration.dart';
 import 'package:dotenv/dotenv.dart' show load, env;
+import 'package:get_it/get_it.dart';
 
 class ApplicationConfig {
   Future<void> _loadEnv() async => load();
@@ -10,6 +12,15 @@ class ApplicationConfig {
 
     // print(variavel);
     // print(flutterHome);
-    _loadDataBaseConfig() {}
+    _loadDataBaseConfig() {
+      final databaseConfig = DatabaseConnectionConfiguration(
+        host: env['DATABASE_HOST'] ?? env['databeseHost']!,
+        user: env['DATABASE_USER'] ?? env['databaseUser']!,
+        port: int.tryParse(env['DATABASE_PORT'] ?? env['databasePort']!) ?? 0,
+        password: env['DATABASE_PASSWORD'] ?? env['databasePassword']!,
+        databaseName: env['DATABASE_NAME'] ?? env['databaseName']!,
+      );
+      GetIt.I.registerSingleton(databaseConfig);
+    }
   }
 }
