ef7e3492c6e2630eadac308decece9cfbce8e311
dariopmaciel <dariodepaulamaciel@hotmail.com>
2024-08-28 18:52:27 -0300

    completando o user service

--- lib/modules/user/service/i_user_service_impl.dart
+++ lib/modules/user/service/i_user_service_impl.dart
@@ -1,18 +1,28 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:cuidapet_shelf/entities/user.dart';
+import 'package:cuidapet_shelf/modules/user/data_repository/i_user_repository.dart';
 import 'package:cuidapet_shelf/modules/user/view_models/user_save_input_model.dart';
+import 'package:injectable/injectable.dart';
 
 import './i_user_service.dart';
 
+@lazySingleton
 class IUserServiceImpl implements IUserService {
+  IUserRepository userRepository;
+  IUserServiceImpl({
+    required this.userRepository,
+  });
+
+
+//objeto de transferencia de camadas
   @override
   Future<User> createUser(UserSaveInputModel user) {
-   
-
-
-
-
-   
+    final userEntity = User(
+      email: user.email,
+      password: user.password,
+      registerType: 'APP',
+      supplierId: user.supplierId,
+    );
+    return userRepository.createUser(userEntity);
   }
- 
-
 }
