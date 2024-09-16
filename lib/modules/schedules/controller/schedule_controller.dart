ccc6b194b882d9bd7508f849be4846b7d7fbe879
dariopmaciel <dariodepaulamaciel@hotmail.com>
2024-09-16 19:04:33 -0300

    atz

--- lib/modules/schedules/controller/schedule_controller.dart
+++ lib/modules/schedules/controller/schedule_controller.dart
@@ -9,7 +9,6 @@ import 'package:shelf_router/shelf_router.dart';
 import 'package:cuidapet_shelf/application/logger/i_logger.dart';
 import 'package:cuidapet_shelf/modules/schedules/service/i_schedule_service.dart';
 import 'package:cuidapet_shelf/modules/schedules/view_models/schedule_save_input_model.dart';
-import 'package:cuidapet_shelf/modules/user/view_models/user_save_input_model.dart';
 
 part 'schedule_controller.g.dart';
 
@@ -33,7 +32,7 @@ class ScheduleController {
         dataRequest: await request.readAsString(),
       );
       await service.scheduleService(inputModel);
-      return Response.ok(jsonEncode(''));
+      return Response.ok(jsonEncode({}));
     } catch (e, s) {
       log.error("ERRO AO SALVAR AGENDAMENTO", e, s);
       return Response.internalServerError();
