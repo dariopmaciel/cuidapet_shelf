c0a0b949ad88572f629ed86c7cd7f728801911f3
dariopmaciel <dariodepaulamaciel@hotmail.com>
2024-09-16 15:25:10 -0300

    monstagem de rota

--- lib/modules/schedules/schedule_router.dart
+++ lib/modules/schedules/schedule_router.dart
@@ -1,12 +1,12 @@
-
 import 'package:cuidapet_shelf/application/routes/i_router.dart';
+import 'package:cuidapet_shelf/modules/schedules/controller/schedule_controller.dart';
+import 'package:get_it/get_it.dart';
 import 'package:shelf_router/shelf_router.dart';
 
-
-class ScheduleRouter implements IRouter{
+class ScheduleRouter implements IRouter {
   @override
   void configure(Router router) {
-    //
+    final scheduleController = GetIt.I.get<ScheduleController>();
+    router.mount('/schedules/', scheduleController.router.call);
   }
-  
 }
