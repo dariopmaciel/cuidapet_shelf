e476f0a049b861387aa6d424cc561d874445e0fd
dariopmaciel <dariodepaulamaciel@hotmail.com>
2024-09-18 15:48:23 -0300

    Mapeio

--- lib/modules/chat/data/i_chat_repository_impl.dart
+++ lib/modules/chat/data/i_chat_repository_impl.dart
@@ -43,22 +43,44 @@ class IChatRepositoryImpl implements IChatRepository {
   }
 
   @override
-  Future<Chat> findChatById(int chat) async {
+  Future<Chat?> findChatById(int chatId) async {
     MySqlConnection? conn;
 
     try {
       conn = await connection.openConnection();
       final result = await conn.query('''
-        SELECT *
+        SELECT 
+          c.id,
+			    c.data_criacao,
+			    c.status,
+          a.nome AS agendamento_nome,
+          a.nome_pet AS agendamento_nome_pet,
+          a.fornecedor_id,
+          a.usuario_id,
+          f.nome AS fornec_nome,
+          f.logo,
+          u.android_token as user_android_token,
+          u.ios_token AS user_ios_token
         FROM chats AS c
         INNER JOIN agendamento a ON a.id = c.agendamento_id
         INNER JOIN fornecedor f ON f.id = a.fornecedor_id
         -- Dados do usuario cliente do petshop
         INNER JOIN usuario u ON u.id = a.usuario_id
-        -- Dados do usuario fornecedor ()o PETSHOP)
+        -- Dados do usuario fornecedor (O PETSHOP)
         INNER JOIN usuario uf ON uf.fornecedor_id = f.id
         WHERE c.id = ?
-      ''', []);
+      ''', [chatId]);
+
+      if (result.isNotEmpty) {
+        return Chat(
+          id: id,
+          user: user,
+          supplier: supplier,
+          nome: nome,
+          petName: petName,
+          status: status,
+        );
+      }
     } on MySqlConnection catch (e, s) {
       log.error('Erro ao buscar dados do CHAT', e, s);
       throw DatabaseException();
